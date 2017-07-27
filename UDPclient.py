12380a964d50f1404573694eac28d290cf4440d4
OlicZ <30511075+OlicZ@users.noreply.github.com>
2017-07-27 18:30:06 -0400

    Add files via upload

---
+++ UDPclient.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+	print "Failed to create socket"
+	sys.exit(1)
+
+print "connection complete!"
+
+host = '127.0.0.1'
+port = 8888
+
+message = ''
+message = raw_input("Enter your message: ")
+access = 1
+while (access == 1):
+	s.sendto(message, (host,port))
+	if message == 'terminate':
+		print "terminated the server, disconnecting"
+		access = 0
+		sys.exit(1)
+	d = s.recvfrom(1024)
+	print 'Received from server: ' + str(d[0])
+	message = raw_input(" Enter your message: ")
+s.close()
---
+++ UDPserver.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+
+host = ''
+port = 8888
+
+try:
+	s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	print "Socket Created"
+except socket.error:
+	print "Failed to create socket"
+	sys.exit(1)
+
+try:
+	s.bind((host,port))
+except socket.error:
+	print "Binding Failed"
+	sys.exit(1)
+
+print "Socket Binding Complete!"
+
+access = ''
+Pass = raw_input("Enter your password: ")
+if (Pass == 'olic'):
+	access = 1
+	
+while (access == 1):
+	print 'Waiting for response'
+	d = s.recvfrom(1024)
+	if d[0] == 'terminate':
+		print "Terminated by user"
+		access = 0
+	data = d[0]
+	addr = d[1]
+
+	if not data:
+		break
+	reply = 'OK...'
+
+	s.sendto(reply, addr)
+	print 'message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+
+s.close
